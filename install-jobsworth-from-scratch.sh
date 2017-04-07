05c8eeb174a7cdd6f7676aea11d49ac1d5714931
Lando <lando.orellana@gmail.com>
2017-04-07 00:35:19 -0300

    Update install-jobsworth-from-scratch.sh
    Added license notice

--- install-jobsworth-from-scratch.sh
+++ install-jobsworth-from-scratch.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#This file is part of the jobsworth-installer program
+#https://github.com/frank-orellana/jobsworth-installer
+#Copyright (C) 2017  Franklin Orellana
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software Foundation,
+#Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+#WARNING:
+#-This scrpit will overwrite info you may have on the jobsworth
+
 echo "Starting installation..." 
 
 pushd `dirname $0` > /dev/null
